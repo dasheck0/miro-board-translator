ede930ebeec05c3e5e56a65fce785fd87e9ce8b3
Stefan Neidig <stefan@rpdev.net>
2022-06-11 15:30:24 +0200

    refactor: move content from index.ts to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
-import {createApp} from 'vue';
-import App from './App.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#root');
+createApp(App).mount("#root");
+
+async function init() {
+  miro.board.ui.on("icon:click", async () => {
+    await miro.board.ui.openPanel({ url: "index.html" });
+  });
+}
+
+init();
