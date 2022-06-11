bce6695f332fda40ed1cae93a4c6b42a4df07e07
Stefan Neidig <stefan@rpdev.net>
2022-06-11 12:32:00 +0200

    feat: add item table to show that items are currently selected

---
+++ src/components/ItemTable.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="item-table">
+    <div class="item-row" v-for="(item, index) in items" :key="index">
+      <div>{{ snakeCaseToSentenceCase(item[0].type) }}</div>
+      <div>{{ item.length }} items</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import groupBy from "lodash.groupby";
+
+export default defineComponent({
+  props: {
+    items: Array as () => any[],
+  },
+  setup(props) {
+    const items = computed(() =>
+      Object.values(groupBy(props.items, "type")).sort((left, right) => {
+        const difference = right.length - left.length;
+
+        if (difference === 0) {
+          return left[0].type.localeCompare(right[0].type);
+        }
+
+        return difference;
+      })
+    );
+
+    console.log("itesm", items);
+
+    return {
+      items,
+    };
+  },
+  methods: {
+    snakeCaseToSentenceCase(snakeCase: string): string {
+      return snakeCase
+        .split("_")
+        .map((comp) => `${comp[0].toUpperCase()}${comp.slice(1).toLowerCase()}`)
+        .join(" ");
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.item-table {
+  .item-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/SelectionDebugComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="selection-debug-component grid">
+    <div class="cs1 ce4 header" >Property</div>
+    <div class="cs5 ce12 header">Value</div>
+
+    <div class="cs1 ce12" v-for="property in getPropertiesBasedOnType" :key="property">
+      <div class="grid">
+        <div class="cs1 ce4">{{ property }}</div>
+        <div class="cs5 ce12">{{ getPropertyValue(property) }}</div>
+      </div>
+    </div>
+
+    {{ component }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    component: {
+      required: true,
+      type: Object as () => Item,
+    },
+  },
+  data() {
+    return {
+      defaultProperties: [
+        "id",
+        "type",
+        "parentId",
+        "x",
+        "y",
+        "width",
+        "height",
+        "origin",
+      ],
+    };
+  },
+  computed: {
+    getPropertiesBasedOnType(): string[] {
+      return {
+        ...this.defaultProperties,
+      };
+    },
+  },
+  methods: {
+    getPropertyValue(property) {
+      console.log("Hfjdhfdjk", property);
+      return this.component[property];
+    },
+  },
+});
+</script>
+
+<style scoped>
+.header {
+    font-weight: bold;
+}
+
+</style>
