23375f8a4f7b3fb6b560ef7591d3e8e590b50f85
Stefan Neidig <stefan@rpdev.net>
2023-09-10 12:27:57 +0200

    fix: uri encode text before sending it to API

--- src/api.ts
+++ src/api.ts
@@ -34,7 +34,9 @@ export default class TranslationAPI {
     }
 
     async translate(text: string, targetLanguage: SupportedLanguages) {
-        const fullUrl = `https://3c4bw77amzjsqi5jcdm7marywq0difgh.lambda-url.eu-central-1.on.aws?auth_key=${this.authKey}&text=${text}&target_lang=${targetLanguage}`;
+        const safeText = encodeURIComponent(text);
+
+        const fullUrl = `https://3c4bw77amzjsqi5jcdm7marywq0difgh.lambda-url.eu-central-1.on.aws?auth_key=${this.authKey}&text=${safeText}&target_lang=${targetLanguage}`;
         const { data } = await axios.get(fullUrl);
 
         return data;
